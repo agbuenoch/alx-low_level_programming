32dca965eb9d64b05052feb3eee8717f507508a7
agbuenoch <agbuenoch38@gmail.com>
2022-09-11 19:05:39 +0000

    A commit message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %d bytes(s)", sizeof(c));
-printf("Size of an int: %d bytes(s)", sizeof(i));
-printf("Size of a long int: %d bytes(s)", sizeof(li));
-printf("Size of long long int: %d bytes(s)", sizeof(lli));
-printf("Size of a float: %d bytes(s)", sizeof(f));
+printf("Size of a char: %d bytes(s)\n", sizeof(c));
+printf("Size of an int: %d bytes(s)\n", sizeof(i));
+printf("Size of a long int: %d bytes(s)\n", sizeof(li));
+printf("Size of long long int: %d bytes(s)\n", sizeof(lli));
+printf("Size of a float: %d bytes(s)\n", sizeof(f));
 return (0);
 }
