9e159b516a914198dae5c86f939679b4081232b8
agbuenoch <agbuenoch38@gmail.com>
2022-09-12 13:56:33 +0000

    A commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,10 @@
  *
  *Return: Always return zero i.e success
  */
+int putchar(int va)
 int main(void)
 {
-
+char values = 'and that piece of art is useful\" - Dora Korpar, 2015-10-19');
+putchar(values);
+return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %lu bytes(s)\n", sizeof(c));
-printf("Size of an int: %lu bytes(s)\n", sizeof(i));
-printf("Size of a long int: %lu bytes(s)\n", sizeof(li));
-printf("Size of a long long int: %lu bytes(s)\n", sizeof(lli));
-printf("Size of a float: %lu bytes(s)\n", sizeof(f));
+printf("Size of a char: %d bytes(s)\n", sizeof(c));
+printf("Size of an int: %d bytes(s)\n", sizeof(i));
+printf("Size of a long int: %d bytes(s)\n", sizeof(li));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(lli));
+printf("Size of a float: %d bytes(s)\n", sizeof(f));
 return (0);
 }
