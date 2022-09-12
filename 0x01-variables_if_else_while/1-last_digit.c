a4fe4619248e0872735717421f208f8501e67180
agbuenoch <agbuenoch38@gmail.com>
2022-09-12 21:45:49 +0000

    A commit msg for Loop task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/*more headers goes there */
 /**
- *main - entry point
+ *main - Entry point
  *
- *return: elways 0 (success)
+ *Return: always 0 (success)
  */
-int main(void) /*The main function*/
+/*The main function*/
+int main(void)
 {
 int n;
 int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
+/*your code goes there */
 last_digit = n % 10;
-if(last_digit > 5)
+if (last_digit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
-if(last_digit == 0)
+if (last_digit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
-if(last_digit < 6 && last_digit != 0)
+if (last_digit < 6 && last_digit != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n , last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
+/*The main return value*/
 return (0);
 }
