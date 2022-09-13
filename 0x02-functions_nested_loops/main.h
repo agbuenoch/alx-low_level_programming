9c413ff38dded9ef9393199fc4d6bcce8eb89c76
agbuenoch <agbuenoch38@gmail.com>
2022-09-13 12:57:07 +0000

    Prototypes of all Functions used in this projects

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,4 @@
-#include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int _putchar(char c);
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
