406bb79f46e27e3b849974ddb5179bd757e689c1
agbuenoch <agbuenoch38@gmail.com>
2022-09-13 22:42:53 +0000

    Pushing everything

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int print_last_digit - check the code
+ *
+ * @x: first parameter
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int x)
+{
+int res = x % 10;
+return (res);
+}
