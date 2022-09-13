511eaaece9f1664108607db5fffcfeef3c45776f
agbuenoch <agbuenoch38@gmail.com>
2022-09-13 15:41:36 +0000

    Pushing everything

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int counter = 0;
+while (counter < 10)
+{
+char val = 'a';
+while (val <= 'z')
+{
+_putchar(val);
+val++;
+}
+_putchar('\n');
+counter++;
+}
+ return;
+}
