3ea7e9920701e70770a4d1b3835f80128da9e8ae
agbuenoch <agbuenoch38@gmail.com>
2022-09-13 22:36:13 +0000

    Pushing everything

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - check the code
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+int res;
+res = a + b;
+return (res);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+  int val = 0;
+  while (counter < 11)
+    {
+      int tim = 0;
+      int a = ;
+      while (tim < 10)
+	{
+	  
+	}
+    }
+}
