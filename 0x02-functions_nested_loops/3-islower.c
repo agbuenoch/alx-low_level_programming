558704c89592c0c580f915efe582f8dbefb7c956
agbuenoch <agbuenoch38@gmail.com>
2022-09-13 19:42:54 +0000

    Pushing everything

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * int _islower - check the code.
+ * _islower - check the code.
  *
  * Return: Always 0.
  */
 int _islower(int c)
 {
- 
-  if (c >= 'a' && c <= 'z')
-    {
-      return 1;
-    }
-  else{ 
-    return 0;
-  }
+if (c >= 'a' && c <= 'z')
+{
+return 1;
+}
+else
+{ 
+return 0;
+}
 }
