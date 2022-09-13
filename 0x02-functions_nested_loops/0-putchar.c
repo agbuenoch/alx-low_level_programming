299dfd9049f29990a5face9a6a21ae53aaf03706
agbuenoch <agbuenoch38@gmail.com>
2022-09-13 12:31:46 +0000

    Functions commits

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * main - entry point
+ *
+ * Return: On success 1.
+ */
+int main(void) /*This is the main function*/
+{
+int _putchar(char c);
+char value;
+value = "_putchar";
+_putchar(value);
+return (0);
+}
