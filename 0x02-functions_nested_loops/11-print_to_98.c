2259ea10735eab818bbeac85756d0ff5e3d2ef1a
agbuenoch <agbuenoch38@gmail.com>
2022-09-14 17:04:36 +0000

    Pushing everything

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,34 +8,35 @@
 void print_to_98(int n)
 {
   
-  if (n < 98)
-    {
-      while (n <= 98)
-	{
-	  printf("%d", n);
-	  if (n < 98)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	  n++;
-	}
-      putchar('\n');
-    }
-  else if (n > 98)
-    {
-      while (n >= 98)
-	{
-	printf("%d", n);
-	if (n > 98)
-	  {
-	    putchar(',');
-	    putchar(' ');
-	  }
-	n--;
-      }
-    }
-  else {
-    printf("%d", n);
-  }
+if (n < 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n < 98)
+{
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+putchar('\n');
+}
+else if (n > 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n > 98)
+{
+putchar(',');
+putchar(' ');
+}
+n--;
+}
+putchar('\n');
+}
+else {
+printf("%d", n);
+}
 }
