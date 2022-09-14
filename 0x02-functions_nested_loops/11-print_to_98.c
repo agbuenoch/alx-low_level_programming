1249ef4cf5b74b9247fa2d892f56ce6beecc7705
agbuenoch <agbuenoch38@gmail.com>
2022-09-14 16:56:29 +0000

    Pushing everything

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,12 @@ void print_to_98(int n)
     {
       while (n <= 98)
 	{
-	  printf("%d, ", n);
+	  printf("%d", n);
+	  if (n < 98)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
 	  n++;
 	}
     }
@@ -20,11 +25,16 @@ void print_to_98(int n)
     {
       while (n >= 98)
 	{
-	printf("%d, ", n);
+	printf("%d", n);
+	if (n > 98)
+	  {
+	    putchar(',');
+	    putchar(' ');
+	  }
 	n--;
       }
     }
   else {
-    printf("%d, ", n);
+    printf("%d", n);
   }
 }
