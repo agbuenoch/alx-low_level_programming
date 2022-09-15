0ff07611241ff5b503218c38f1b986aa72438c4d
agbuenoch <agbuenoch38@gmail.com>
2022-09-15 19:48:42 +0000

    changes made

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 	  else
 	    {
 	      /*	_putchar((row / 10) + '0'); */
-	      _putchar((col % 10) + '0');
+	      _putchar((row / 10) + '0');
 	    }
 	  _putchar(',');
 	  _putchar(' ');
