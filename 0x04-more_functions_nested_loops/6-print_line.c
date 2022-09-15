c37abff39bcb7ab209ad3772415eb5dd8f14929b
agbuenoch <agbuenoch38@gmail.com>
2022-09-15 20:21:36 +0000

    Headerfile for - More functions projects

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,15 @@
  */
 void print_line(int n)
 {
-  if (n > 0)
-    {
-      int i;
-      for (i = 0; i <= n; i++)
-	{
-	  _putchar('_');
-	}
-      _putchar('\n');
-    }
-  else
-    _putchar('\n');
+if (n > 0)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
 }
