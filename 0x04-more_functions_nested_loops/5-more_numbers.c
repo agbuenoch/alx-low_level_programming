f18f1a568ecc1dc9f44b3551f9b9039d22d11677
agbuenoch <agbuenoch38@gmail.com>
2022-09-15 19:33:39 +0000

    Headerfile for - More functions projects

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,25 +6,26 @@
  */
 void more_numbers(void)
 {
-  int row;
-  row = 1;
-  while (row <= 10){
-    int col;
-    col = 0;
-    while (col <= 14)
-      {
-	if (col <= 9)
-	  {
-	    _putchar(col + '0');
-	  }
-	else
-	  {
-	    _putchar((col / 10) + '0');
-	    _putchar((col % 10) + '0');
-	  }
-	col++;
-      }
-    row++;
-    _putchar('\n');
-  }
+int row;
+row = 1;
+while (row <= 10)
+{
+int col;
+col = 0;
+while (col <= 14)
+{
+if (col <= 9)
+{
+_putchar(col + '0');
+}
+else
+{
+_putchar((col / 10) + '0');
+_putchar((col % 10) + '0');
+}
+col++;
+}
+row++;
+_putchar('\n');
+}
 }
