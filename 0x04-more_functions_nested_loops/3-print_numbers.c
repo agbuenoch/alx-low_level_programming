e94cf760ea63ede8541865be95795ca1395c21ac
agbuenoch <agbuenoch38@gmail.com>
2022-09-15 11:09:01 +0000

    Headerfile for - More functions projects

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+i = 0;
+while (i <= 9)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
