34d11d01e80fe5ccf93c60b43d5f2ffb227cdd9a
agbuenoch <agbuenoch38@gmail.com>
2022-09-15 22:40:47 +0000

    Headerfile for - More functions projects

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,19 +5,17 @@
 int main(void)
 {
   int i;
-  i = 1;
-  while (i <= 100)
+  for (i = 1; i <= 100; i++)
     {
             
-      if (i % 3 == 0)
+      if (i % 15 == 0)
+	  printf("FizzBuzz ");
+      else if (i % 3 == 0)
 	  printf("Fizz ");
       else if (i % 5 == 0)
 	  printf("Buzz ");
-      else if (i % 15 == 0)
-	  printf("FizzBuzz ");
       else
 	  printf("%d ", i);
-      i++;
     }
   printf("\n"); /*After displaying the result move to next line*/
   return (0);
