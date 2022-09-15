15906e8059b3b742df0d97ee2c5f7af5a23d9557
agbuenoch <agbuenoch38@gmail.com>
2022-09-15 21:06:33 +0000

    Headerfile for - More functions projects

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code
+ *
+ * @n: first parameter
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+  if (n > 0)
+    {
+      int i;
+      for (i = 0; i <= n; i++)
+	{
+	  for ()){
+	     
+          }
+	  _putchar('\\');
+	}
+      _putchar('\n');
+    }
+    else
+       {
+	 _putchar('\n');
+       }
+}
