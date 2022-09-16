398891e232044d795c0aa8598160ca31544bdb32
agbuenoch <agbuenoch38@gmail.com>
2022-09-16 23:49:38 +0000

    A commit msg for Loop task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,22 @@ int main(void){
 
   int num1, num2;
   /*ASCII code for 0*/
-  num1 = 48;
+  num1 = 48; /*Initialized num1 = 0. ASCII value of zero-0 is 48*/
 
-  while (num1 <= 57)
+  while (num1 <= 57) /*while num1 <= 9. ASCII value of Nine-9 is 57*/
     {
       num2=48;
       while (num2 <= 57)
 	{
-	  putchar(num1);
-	  putchar(num2);
-	  if (num2 < 57)
+	  if (((num1 + num2) - (num2 + num1)) == 0)
 	    {
-	      putchar(',');
-	      putchar(' ');
+	     putchar(num1);
+	     putchar(num2);
+	     if (num2 < 57)
+	       {
+	         putchar(',');
+	         putchar(' ');
+	       }
 	    }
 	  num2++;
 	}
