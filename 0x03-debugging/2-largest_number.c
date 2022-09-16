ae48cf3ca869589bd19030eb5edcee0423e306b3
agbuenoch <agbuenoch38@gmail.com>
2022-09-16 09:27:58 +0000

    Debugging Commit Msg

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,27 @@ int largest_number(int a, int b, int c)
 {
   int largest;
 
-  if (a > b && b > c)
+  if (a > b)
     {
-      largest = a;
+      if (a > c)
+	{
+	  largest = a;
+	}
+      else
+	{
+	  largest = c;
+	}
     }
-  else if (b > a && a > c)
+  else
     {
-      largest = b;
-    }
-  else if (c > a  && a > b)
-    {
-      largest = c;
+      if (b > c)
+	{
+	  largest = b;
+	}
+      else
+	{
+	  largest = c;
+	}
     }
 
   return (largest);
