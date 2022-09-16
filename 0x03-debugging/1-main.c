689f8efadbb2e8576119aca994a61e404a4ef319
agbuenoch <agbuenoch38@gmail.com>
2022-09-16 09:11:12 +0000

    Debugging Commit Msg

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - causes an infinite loop
- * Return: 0
+ * main - The entry point
+ *
+ * Description:  A function that print two statement,
+ * to the standard output and resolve an infinite loop.
+ * @\\: first parameter
+ * @o/: second parameter
+ * @\n: third parameter
+ *
+ * Return: 0 (successs)
  */
-
 int main(void)
 {
-  int i;
-
-  printf("Infinite loop incoming :(\n");
-  /**
-   *i = 0;
-   *
-   * while (i < 10)
-   *{
-   *  putchar(i);
-   *}
-   */
-  printf("Infinite loop avoided! \\o/\n");
-
-  return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+/**
+*i = 0;
+*
+* while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
