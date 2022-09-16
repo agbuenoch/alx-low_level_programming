7cc81aa2e1b10a5d82a25a89abc16418361b2a51
agbuenoch <agbuenoch38@gmail.com>
2022-09-16 23:32:41 +0000

    A commit msg for Loop task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,64 +4,29 @@
  *
  *Return: always 0 (success)
  */
-int main(void) /*The main function*/
-{
-int x[10];
-int y[10];
+int main(void){
 
- x[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
- y[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
- 
- for (countA = x[0]; countA < x.length, countA++)
-   {
-     for (countB = y[0]; countB < y.length, countB++)
-       {
-	 if ((x[countA] != y[countB]) || !(x[countA]y[countB] == y[countB]x[countA]))
-	   {
-	     putchar(x[countA]y[countB]);
-	   }
-       }
-   }
+  int num1, num2;
+  /*ASCII code for 0*/
+  num1 = 48;
 
-/**
-for(x = 0; x < 10; x++)
-   {
-     for(y = 0; y < 10; y++)
-       {
-	 if (!(x == y) || (y < x))
-	   {
-	 putchar(x + '0');
-	 putchar(y + '0');
-	 if ((x + y) <= 17)
-	   {
-	     putchar(',');
-	     putchar(' ');
-	   }
-	   }
-       }
-   }
- putchar('\n');
- return (0);
+  while (num1 <= 57)
+    {
+      num2=48;
+      while (num2 <= 57)
+	{
+	  putchar(num1);
+	  putchar(num2);
+	  if (num2 < 57)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	  num2++;
+	}
+      num1++;
+    }
+  putchar('\n');
+  return (0);
+
 }
-
-
-
-
-
-
-
-  
-while (digit < 10)
-{
-putchar(digit + '0');
-if (digit < 9)
-{
-putchar(',');
-putchar(' ');
-}
-digit++;
-}
-putchar('\n');
-return (0);
-}
- */
