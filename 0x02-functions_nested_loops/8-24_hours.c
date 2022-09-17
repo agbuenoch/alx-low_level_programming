e6a7ee9117a89b3925e24d2ea68dcd3dfea88606
agbuenoch <agbuenoch38@gmail.com>
2022-09-17 21:45:47 +0000

    Pushing everything

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -30,19 +30,19 @@ void jack_bauer(void)
 {
 int hour1;
 hour1 = 48;
-while (hour1 < 51)
+while (hour1 <= 50)
 {
 int hour2;
 hour2 = 48;
-while (hour2 < 52)
+while (hour2 <= 51)
 {
 int min1;
 min1 = 48;
-while (min1 < 54)
+while (min1 <= 53)
 {
 int min2;
 min2 = 48;
-while (min2 < 58)
+while (min2 <= 57)
 {
 _putchar(hour1);
 _putchar(hour2);
