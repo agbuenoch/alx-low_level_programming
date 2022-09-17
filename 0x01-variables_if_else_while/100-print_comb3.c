925b109dd3ca0099b2f76465ef6f6319315fde8b
agbuenoch <agbuenoch38@gmail.com>
2022-09-17 00:19:31 +0000

    A commit msg for Loop task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,19 +12,14 @@ int main(void){
 
   while (num1 <= 57) /*while num1 <= 9. ASCII value of Nine-9 is 57*/
     {
-      num2=48;
+      num2 = 49; /* num2 = 1. ASCII value of One-1 is 49*/
       while (num2 <= 57)
 	{
-	  if (((num1 + num2) - (num2 + num1)) == 0)
-	    {
 	     putchar(num1);
 	     putchar(num2);
-	     if (num2 < 57)
-	       {
-	         putchar(',');
+	 
+	       putchar(',');
 	         putchar(' ');
-	       }
-	    }
 	  num2++;
 	}
       num1++;
