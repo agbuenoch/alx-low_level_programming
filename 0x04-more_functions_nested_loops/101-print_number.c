dc35188854f2262446f0c59a63f349896eaf71a6
agbuenoch <agbuenoch38@gmail.com>
2022-09-17 05:11:51 +0000

    Headerfile for - More functions projects

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,43 +9,20 @@
  */
 void print_number(int n)
 {
-  /*
-  int arr_size[];
-  arr_size = "n";
-  */
+  /* If number is smaller than 0, put a - sign */
+     /* and change number to positive */
+  if (n < 0) {
+    _putchar('-');
+    n = -n;
+  }
+
+  /* Remove the last digit and recur */
+  if (n/10)
+    _putchar(n/10);
+
+    /* Print the last digit */
+  _putchar(n%10 + '0');
   
-    
-  char val[4];
-  val[4] = "n";
-  
-  if (n <= 9)
-    {
-      _putchar(n + 'n');
-    }
-  else if (n >= 10 && n <= 99)
-    {
-      _putchar((n / 10) + '0');
-      _putchar((n % 10) + '0');
-    }
-  else if (n >= 100 && n <= 999)
-    {
-      int i;
-      _putchar((n / 100) + '0');
-      for (i = 1; i <= 1; i++) /* i != '\0' READS--> i NOT EQUAL TO EOF (End Of File)*/
-	{
-	  _putchar(val[1]);
-	}
-      _putchar((n % 10) + '0');
-    }
-  else
-    {
-      int i;
-      _putchar((n / 1000) + '0');
-      for (i = 1; i <= 2; i++)
-	{
-	  _putchar(val[1]);
-	  _putchar(val[2]);
-	}
-      _putchar((n % 10) + '0');
-    }
 }
+  
+
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
- * 
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-  int i;
-  for (i = 1; i <= 100; i++)
-    {
-            
-      if (i % 15 == 0)
-	  printf("FizzBuzz ");
-      else if (i % 3 == 0)
-	  printf("Fizz ");
-      else if (i % 5 == 0)
-	  printf("Buzz ");
-      else
-	  printf("%d ", i);
-    }
-  printf("\n"); /*After displaying the result move to next line*/
-  return (0);
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+printf("\n"); /*After displaying the result move to next line*/
+return (0);
 }
