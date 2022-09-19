516054b16baee2e084b1e7867c2eaabbe175ee39
agbuenoch <agbuenoch38@gmail.com>
2022-09-19 19:46:43 +0000

    Pointer and Array commits

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ *
+ * @a: first pointer-to-integer parameter
+ * @b: second pointer-to-integer parameter
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
