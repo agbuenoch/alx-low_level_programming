92e922467ef25a24f2daccb723fcadb479e4aaec
agbuenoch <agbuenoch38@gmail.com>
2022-09-19 21:12:04 +0000

    Pointer and Array commits

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,11 @@
  */
 int _strlen(char *s)
 {
-  int i;
-  int len;
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      len = i + 1;
-    }
-  return len;
+int i;
+int len;
+for (i = 0; s[i] != '\0'; i++)
+{
+len = i + 1;
+}
+return len;
 }
