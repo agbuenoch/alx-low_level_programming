02a2ebeaaec129d658f39bf5beda2badafd23a20
agbuenoch <agbuenoch38@gmail.com>
2022-09-19 19:30:11 +0000

    Pointer and Array commits

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- * reset_to_98 - Update the value of a variable 
+ * reset_to_98 - Update the value of a variable
+ *
+ *@n: Pointer-to-integer parameter
  *
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
