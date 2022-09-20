cc1c07b286734379de35f9809f474a88fb95db29
agbuenoch <agbuenoch38@gmail.com>
2022-09-20 19:10:37 +0000

    Pointer and Array commits

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,17 @@
 */
 void puts_half(char *str)
 {
-  int i;
-  int half;
-  int array_length = 0;
-  while (str[array_length] != '\0')
-    {
-      array_length++;
-    }
-  half = (array_length - 1) / 2;
-
-  for (i = half + 1; i <= array_length - 1; i++)
-    {
-      _putchar(str[i]);
-    }
-  _putchar('\n');  
+int i;
+int half;
+int array_length = 0;
+while (str[array_length] != '\0')
+{
+array_length++;
+}
+half = (array_length - 1) / 2;
+for (i = half + 1; i <= array_length - 1; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
