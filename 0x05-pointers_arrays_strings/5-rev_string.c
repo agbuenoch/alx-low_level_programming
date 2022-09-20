b30ba421fd628bb21965796e1ecb2c21a94e74da
agbuenoch <agbuenoch38@gmail.com>
2022-09-20 12:32:38 +0000

    Pointer and Array commits

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,18 @@
 #include "main.h"
 /**
- * rev_string - Printing string in reverse order.
+ * rev_string - print strings in reverse order
  *
- * @s: first pointer-to-character parameter.
+ * @s: first pointer-to-character parameter
  *
  * Return: Always 0.
  */
 void rev_string(char *s)
 {
-  int i;
-  int length;
-  length = 10;
-
-  /* Traverse string from end */
-  for (i = length - 1; i >= 0; i--) {
-    if (s[i] == ' ') {
-
-      /* putting the NULL character at the */
-      /* position of space characters for */
-      /* next iteration. */
-      s[i] = '\0';
-
-      /* Start from next character */
-      _putchar(s[i]);
-    }
-  }
-  
-  _putchar('\n');
+int i;
+int array_length = 0;
+while (s[array_length] != '\0')
+array_length++;
+for (i = array_length - 1; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - print all the even number
+ *
+ * @str: first pointer-to-character parameter
+*/
+void puts2(char *str)
+{
+  int i;
+
+  for (i = 0; i != '\0'; i++)
+    {
+      if (str[i] % 2 == 0)
+	  _putchar(str[i]);
+    }
+  _putchar('\n');
+}
