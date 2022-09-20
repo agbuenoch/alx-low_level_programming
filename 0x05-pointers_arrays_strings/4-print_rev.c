b83ad0211c6b3309dfc7cea7427992a4e5ac67a1
agbuenoch <agbuenoch38@gmail.com>
2022-09-20 11:45:57 +0000

    Pointer and Array commits

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
 /**
  * print_rev - print strings in reverse order
  *
@@ -10,11 +9,9 @@
  */
 void print_rev(char *s)
 {
-  int length = strlen(s);
-  
-  int i;
-  for (i = length - 1; i >= 0; i--) {
-      _putchar(s[i]);
-  }
-  _putchar('\n');
+int length = strlen(s);
+int i;
+for (i = length - 1; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
 }
