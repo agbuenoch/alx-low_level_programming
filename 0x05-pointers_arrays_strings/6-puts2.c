bdd25146c7a5a3dbeeb1c614b0184670149f95b9
agbuenoch <agbuenoch38@gmail.com>
2022-09-20 15:15:14 +0000

    Pointer and Array commits

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,16 @@
 */
 void puts2(char *str)
 {
-  int i;
-  int counter = 0;
-  while (str[counter] != '\0')
-    counter++;
-  
-  for (i = 0; i <= counter; i++)
-    {
-      if (str[i] % 2 == 0)
-	{
-	  _putchar(str[i]);
-	}
-    }
-  _putchar('\n');
+int i;
+int counter = 0;
+while (str[counter] != '\0')
+counter++;
+for (i = 0; i <= counter; i++)
+{
+if (str[i] % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
 }
