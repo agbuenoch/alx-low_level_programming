9c40eb446d7a701248fef1fca215a451229505b2
agbuenoch <agbuenoch38@gmail.com>
2022-09-21 07:54:14 +0000

    Pointer and Array commits

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,18 +2,19 @@
 /**
  * *_strcpy - copy from one string to another
  *
- * @*dest: first pointer-to-character parameter
- * @*src: second pointer-to-character parameter
+ * @dest: first pointer-to-character parameter
+ * @src: second pointer-to-character parameter
  *
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
 {
-  int i;
-  for (i = 0; src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-  dest[i] = '\0';
-  return (dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+/*Makes sure that the string is NULL terminated*/
+dest[i] = '\0';
+return (dest);
 }
