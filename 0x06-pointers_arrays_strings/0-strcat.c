fbf6e47f1d4d1dca5f6f357f4a6adbf67c6e9523
agbuenoch <agbuenoch38@gmail.com>
2022-09-21 21:56:09 +0000

    More pointers commit msg

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,17 @@
 */
 char *_strcat(char *dest, char *src)
 {
-  int i;
-  int length = 0;
-  while (dest[length] != '\0')
-    {
-      length++;
-    }
-
-  for (i = 0; src[i] != '\0'; i++, length++)
-    {
-      dest[length] = src[i];
-    }
-  dest[length] = '\0';
-  
-  return (dest);
+int i;
+int length = 0;
+while (dest[length] != '\0')
+{
+length++;
+}
+for (i = 0; src[i] != '\0'; i++, length++)
+{
+dest[length] = src[i];
+}
+/*Make sure that the string is NULL terminated*/
+dest[length] = '\0';
+return (dest);
 }
