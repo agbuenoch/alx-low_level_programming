d21d584450003cc572579f55dbd1abbf692e8913
agbuenoch <agbuenoch38@gmail.com>
2022-09-22 08:58:14 +0000

    More pointers commit msg

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,9 +15,9 @@ for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
- for ( ; i < n; i++)
-   {
-     dest[i] = '\0';
-   }
+for ( ; i < n; i++)
+{
+dest[i] = '\0';
+}
 return (dest);
 }
