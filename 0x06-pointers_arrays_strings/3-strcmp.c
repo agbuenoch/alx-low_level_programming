f8f62005c624285ea9dc41067abe653bcaef1cf1
agbuenoch <agbuenoch38@gmail.com>
2022-09-22 09:19:35 +0000

    More pointers commit msg

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,15 +14,15 @@ int _strcmp(char *s1, char *s2)
     {
       if (s1[i] < s2[i])
 	{
-	  return (s1[i] < s2[i]);
+	  return (-15);
 	}
       else if (s1[i] > s2[i])
 	{
-	  return (s1[i] > s2[i]);
+	  return (15);
 	}
       else
 	{
-	  return (s1[i] == s2[i]);
+	  return (0);
 	}
     }
   return (0); 
