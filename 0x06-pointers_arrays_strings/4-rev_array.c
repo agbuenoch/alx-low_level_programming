01ff16f22461694438db5f4af362737c18413296
agbuenoch <agbuenoch38@gmail.com>
2022-09-23 21:55:45 +0000

    More pointers commit msg

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,9 +8,14 @@
 void reverse_array(int *a, int n)
 {
   int i;
-  for (i = n; i >= 0; i--)
+  for (i = n - 1; i >= 0; i--)
     {
       _putchar(a[i] + '0');
+      if (i > 0)
+	{
+	  _putchar(',');
+	  _putchar(' ');
+	}
     }
   _putchar('\n');
 }
