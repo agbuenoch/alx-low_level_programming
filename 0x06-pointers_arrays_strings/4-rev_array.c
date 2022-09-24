314ed692747c00cb951fd63c1b6fa9116a07baa4
agbuenoch <agbuenoch38@gmail.com>
2022-09-24 15:07:16 +0000

    More pointers commit msg

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,12 +5,23 @@
  * @a: first pointer-to-integer parameter
  * @n: second parameter
 */
-void vv(int val)
+/*A Function to print long string of integer using `putchar`*/
+void vv(long val)
 {
   if (val != 0)
     {
       vv(val / 10);
       _putchar((val % 10) + '0');
+    }
+}
+/*A Function for inserting `comma and space`*/
+void comma(int len)
+{
+  int i = 0;
+  if (i < len - 1)
+    {
+      _putchar(',');
+      _putchar(' ');
     }
 }
 void reverse_array(int *a, int n)
@@ -21,10 +32,12 @@ void reverse_array(int *a, int n)
       if (i <= 9)
 	{
 	  _putchar(a[i] + '0');
+	  comma(n); /*Put comma follow by space after each number except the last number*/
 	}
       else
 	{
 	  vv(a[i]);
+	  comma(n);
 	}
     }
   _putchar('\n');
