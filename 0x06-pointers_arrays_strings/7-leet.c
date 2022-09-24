80cac660a501e514590baef2e3d427980416e6b5
agbuenoch <agbuenoch38@gmail.com>
2022-09-24 16:21:47 +0000

    More pointers commit msg

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * leet - substitutes character with numbers.
+ *
+ * @str: first pointer-to-character parameter.
+ *
+ * Return: pointer-to-character
+*/
+char *leet(char *str)
+{
+  int i;
+  for (i = 0; str[i] != '\0'; i++)
+    {
+      str[i]
+    }
+  
+  return str;
+}
