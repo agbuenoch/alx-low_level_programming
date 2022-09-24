72075c7fddcbbf0bed600e5376e676bb01e707aa
agbuenoch <agbuenoch38@gmail.com>
2022-09-24 16:52:56 +0000

    More pointers commit msg

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,23 +7,28 @@ void recur(int num)
   if (num != 0)
     {
       recur(num / 10);
-      _putchar(num % 10 + '0');
+      _putchar((num % 10) + '0');
     }
+  /*
+  if (num < 0)
+    {
+      sign = num / 1;
+      _putchar('-');
+      
+    }*/
 }
 void print_number(int n)
 {
-  if (n <= 9)
+  if (n < 0)
+    {
+      _putchar('-');
+    }
+  else if (n == 0 || n <= 9)
     {
       _putchar(n + '0');
     }
   else
     {
-      /*
-      if (n != 0)
-	{
-	   n = n / 10;
-	   _putchar((n % 10) + '0');
-	   }  */
-      recur(n);
+       recur(n);
     }
 }
