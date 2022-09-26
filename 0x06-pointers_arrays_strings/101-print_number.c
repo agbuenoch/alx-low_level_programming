aa2845080128a821e391c1301e82716142e47b3c
agbuenoch <agbuenoch38@gmail.com>
2022-09-26 07:55:46 +0000

    More pointers commit msg

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,40 +1,27 @@
 #include "main.h"
 /**
-
+ * print_number - prints integers like: 7, -7, 63, -987
+ *
+ * @n: first parameter
+ *
+ * Return: zero.
 */
-/*
-void recur(int num)
-{
-  if (num < 0)
-    {
-      _putchar('-');
-      num = -num;
-    }
-  if (num != 0)
-    {
-      recur(num / 10);
-      _putchar((num % 10) + '0');
-    }
-
-    } */
-
 
 void print_number(int n)
 {
-  if (n < 0)
-    {
-      _putchar('-');
-      n = -n;
-    }
-  
-  if (n <= 9)
-    {
-      _putchar(n + '0');
-    }
-  else
-    {
-      if (n / 10)
-	print_number(n / 10);
-      _putchar((n % 10) + '0');
-    }
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n <= 9)
+{
+_putchar(n + '0');
+}
+else
+{
+if (n / 10)
+print_number(n / 10);
+_putchar((n % 10) + '0');
+}
 }
