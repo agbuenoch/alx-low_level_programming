097388cc5ba6af607905d257f4bdc37a6532ccd8
agbuenoch <agbuenoch38@gmail.com>
2022-09-27 13:42:07 +0000

    Pointer-to-Pointer Commits

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,21 @@
 #include "main.h"
 /**
+ * _strchr -  locates a character in a string
  *
+ * @s: first pointer-to-character parameter
+ * @c: second parameter
  *
- *
- *
+ * Return: pointer-to-parameter s.
 */
 char *_strchr(char *s, char c)
 {
-
-
-
+  int i;
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      if (c == s[i])
+	{
+	  ;
+	}
+    }
+  return (s);
 }
