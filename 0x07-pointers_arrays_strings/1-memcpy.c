4dd91e23e5d25da5af65d62ce9d3bcc84de5d34e
agbuenoch <agbuenoch38@gmail.com>
2022-09-27 13:27:20 +0000

    Pointer-to-Pointer Commits

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,10 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int i;
-  for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-
-  return dest;
+unsigned int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+return dest;
 }
