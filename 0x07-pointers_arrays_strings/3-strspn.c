7e0738455b36b36bf9c877ca9b089dc07f9d4949
agbuenoch <agbuenoch38@gmail.com>
2022-09-27 16:08:05 +0000

    Pointer-to-Pointer Commits

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,13 +12,21 @@ unsigned int _strspn(char *s, char *accept)
   int i;
   unsigned int count;
   count = 0;
-  for (i = 0; s[i] < ' '; i++)
+  for (i = 0; s[i] < '\0'; i++)
     {
-      if (accept[i] == s[i])
+      int j; 
+      for (j = 0; accept[j] != '\0'; j++)
+	{
+	  if (s[i] == accept[i])
 	 {
 	   count++;
 	   break;
 	 }
+	}
+      if (s[i] != accept[i])
+	{
+	  return count;
+	}
     }
 
   return (count);
