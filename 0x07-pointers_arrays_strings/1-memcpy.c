4c9e1018eff302e04fdda1641e07b649ff9020e2
agbuenoch <agbuenoch38@gmail.com>
2022-09-27 13:24:49 +0000

    Pointer-to-Pointer Commits

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,20 @@
 #include "main.h"
 /**
+ * _memcpy - copies memory area
  *
+ * @dest: first pointer-to-character
+ * @src: second pointer-to-character
+ * @n: third parameter
  *
- *
+ * Return: dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+  unsigned int i;
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+      dest[i] = src[i];
+    }
 
-
-
+  return dest;
 }
