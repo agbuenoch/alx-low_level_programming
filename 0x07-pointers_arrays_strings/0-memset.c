130258a243eaea5af40a1477b2110a6079f58b90
agbuenoch <agbuenoch38@gmail.com>
2022-09-27 12:45:13 +0000

    Pointer-to-Pointer Commits

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
+ * _memset - fills memory with a constant bytes
  *
+ * @s: pointer-to-character
+ * @b: second parameter
+ * @n: third parameter
  *
- *
+ * Return: pointer-to-character
 */
 char *_memset(char *s, char b, unsigned int n)
 {
+unsigned  int i;
+  for (i = 0; i < n; i++)
+    {
+      s[i] = b;
+      printf("%c", s[i]);
+      /* _putchar(s[i]); */
+    }
 
-
-
+  return (s);
 }
