b58dbcda6af9c612e106b9e0bf20fbcc6e20a59f
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 19:22:54 +0000

    recursion commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,9 @@
  *
  * Return: zero
 */
-int _strlen_recursion(char *s){
-
-  if (*s != '\0')
-    return 1 + _strlen_recursion(s + 1);
-  return (0);
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+return (0);
 }
