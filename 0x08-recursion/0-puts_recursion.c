d7dd31c5c23bf01505c42a690249e590776ee59b
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 10:53:19 +0000

    recursion commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,12 +4,13 @@
  *
  * @s: pointer-to-character parameter
  *
- * Return: zero
+ * Return: nothing
 */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
+  _putchar('\n');
   return;
 }
 _putchar(*s);
