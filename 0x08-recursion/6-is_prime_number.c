243d8f97c36ca8d0dc3fc5168ccb17367bb80a4b
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 22:00:04 +0000

    recursion commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
+ * is_prime_number - returns 1 if the input integer is a
+ * prime number, otherwise return 0
  *
  * @n: integer parameter n
  *
@@ -8,33 +9,31 @@
 */
 int is_prime_number(int n)
 {
-
-  if (n == 0)
-    {
-	  return (0);
-    }
-  if (n == 1)
-    {
-      return (0);
-    }
-
-  if (n % 4 == 0)
-    {
-      return (0);
-    }
-  else
-    {
-      if (n >= 9)
-	{
-	  if ((n % 3 == 0 || n % 5 == 0 || n % 7 == 0))
-	    {
-	      return (0);
-	    }
-	  else
-	    {
-	      return (1);
-	    }
-	}
-    }
-  return (0);
+if (n == 0)
+{
+return (0);
+}
+if (n == 1)
+{
+return (0);
+}
+if (n % 4 == 0)
+{
+return (0);
+}
+else
+{
+if (n >= 9)
+{
+if ((n % 3 == 0 || n % 5 == 0 || n % 7 == 0))
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
+}
+return (0);
 }
