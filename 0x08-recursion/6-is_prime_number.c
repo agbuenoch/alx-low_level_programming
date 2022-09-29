a6a2d8390589df5f156e3829d92c0a3951352382
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 21:43:58 +0000

    recursion commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,17 +9,32 @@
 int is_prime_number(int n)
 {
 
-  if (n == 2)
+  if (n == 0)
     {
-	  return (1);
+	  return (0);
     }
-  else if (n % 2 == 0)
+  if (n == 1)
     {
-      
+      return (0);
+    }
+
+  if (n % 4 == 0)
+    {
+      return (0);
     }
   else
     {
-      return (0);
+      if (n >= 9)
+	{
+	  if ((n % 3 == 0) && ((n % 5 == 0) || (n % 7 == 0)))
+	    {
+	      return (0);
+	    }
+	  else
+	    {
+	      return (1);
+	    }
+	}
     }
   return (0);
 }
