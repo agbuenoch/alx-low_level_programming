41261dc1ab300d208a2bb17f5802972bc45eb577
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 11:12:11 +0000

    recursion commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _print_rev_recursion - print a string in reverse
  *
@@ -8,10 +9,12 @@
 */
 void _print_rev_recursion(char *s)
 {
-  if (*s == '\0')
+  if (*s == 0){
+    _putchar('\n');
     return;
-  _putchar(*s);
-  _print_rev_recursion(--s);
-
-
+  }
+  if (*s < '\0'){
+    printf("%c", *s);
+  _print_rev_recursion(s--);
+  }
 }
