3bea692e116a3d481a6a36bda67c9902ffc602ea
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 20:05:47 +0000

    recursion commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,23 +9,21 @@
 */
 int _pow_recursion(int x, int y)
 {
-  
-  if (y < 0)
-    {
-      return (-1);
-    }
-  else if (y == 0)
-    {
-      return (1);
-    }
-  else if (x == 0)
-    {
-      return (0);
-    }
-  else
-    {
-      return (x*_pow_recursion(x, y - 1));
-    }
-
-  return (0);
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else if (x == 0)
+{
+return (0);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+return (0);
 }
