ccc6098a5c670b92bc4af70a2589662732eb1333
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 22:51:41 +0000

    recursion commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *
+ * @s: first pointer-to-character
+ *
+ * Return: zero
+*/
+int int is_palindrome(char *s)
+{
+  int j;
+  j = 0;
+  
+ if (*s != '\0')
+    {
+      j++;
+    }
+  if (*s == s[j] && *s != '\0')
+    {
+      is_palindrome(s + 1);
+      j--;
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
