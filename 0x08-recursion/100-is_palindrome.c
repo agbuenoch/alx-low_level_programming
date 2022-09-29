892129ad26e5679e794a8bfd080fbad91723f4fa
agbuenoch <agbuenoch38@gmail.com>
2022-09-29 22:58:04 +0000

    recursion commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,23 @@
 #include "main.h"
 /**
- * wildcmp - compares two strings and returns 1 if the
- * strings can be considered identical, otherwise return 0
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
  *
- * @s1: first pointer-to-character
- * @s2: second pointer-to-character
+ * @s: first pointer-to-character
  *
  * Return: zero
 */
-int wildcmp(char *s1, char *s2)
+int is_palindrome(char *s)
 {
   int j;
+  j = 0;
   
- if (*s2 != '\0')
+ if (*s != '\0')
     {
       j++;
     }
-  if (*s1 == s2[j] && *s1 != '\0')
+  if (*s == s[j])
     {
-      s1 + 1;
+      is_palindrome(s + 1);
       j--;
       return (1);
     }
