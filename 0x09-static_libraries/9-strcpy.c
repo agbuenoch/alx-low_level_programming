fd8e1553ed81db9db8eccf0680b6f921070a6a5b
agbuenoch <agbuenoch38@gmail.com>
2022-09-30 15:05:07 +0000

    static libraries commits

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcpy - copy string to another string
+ *
+ * @dest: first pointer-to-character
+ * @src: second pointer-to-character
+ *
+ * Return: pointer-to-character
+*/
+char *_strcpy(char *dest, char *src)
+{
+return (dest);
+}
