757be85b86b2092e7dd41166af18b0a70d63d57b
agbuenoch <agbuenoch38@gmail.com>
2022-09-30 14:57:52 +0000

    static libraries commits

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strstr - 
+ *
+ * @haystack: first pointer-to-character
+ * @needle: second pointer-to-character
+ *
+ * Return: pointer-to-character
+*/
+char *_strstr(char *haystack, char *needle)
+{
+return (haystack);
+}
