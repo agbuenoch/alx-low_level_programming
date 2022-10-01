79ec65536cce50510271930b5b00543f6a101142
agbuenoch <agbuenoch38@gmail.com>
2022-10-01 17:42:16 +0000

    argc and argv commits

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print all arguement passed to main function
+ *
+ * @argc: integer parameter
+ * @argv: pointer-to-pointer array parameter
+ *
+ * Return: zero
+*/
+int main(int argc, char **argv)
+{
+int i;
+int mul = 1;
+for (i = 1; i < argc; i++)
+{
+  mul *= atoi(argv[i]);
+}
+printf("%d\n", mul);
+return (0);
+}
