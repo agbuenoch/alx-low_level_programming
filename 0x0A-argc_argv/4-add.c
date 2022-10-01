75857b6849900071894f43dad336e3549195b2ef
agbuenoch <agbuenoch38@gmail.com>
2022-10-01 21:30:41 +0000

    argc and argv commits

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,27 +12,29 @@ int main(int argc, char **argv)
 {
 int i;
 int sum = 0;
- if (argc < 1)
-   {
-     printf("%d\n", sum);
-   }
- else
-   {
-     int j;
-     for (i = 1; i < argc; i++)
-       {
-	 for (j = 0; j < i - 1; j++){
-	 if (!(atoi(argv[j]) / 1))
-	   {
-	     printf("Error\n");
-	     return (0);
-	   }
-	 else
-	   {
-	     sum += atoi(argv[i]);
-	   }}
-       }
-   }
+if (argc < 1)
+{
+printf("%d\n", sum);
+}
+else
+{
+int j;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; j < i - 1; j++)
+{
+if (!(atoi(argv[j]) / 1))
+{
+printf("Error\n");
+return (0);
+}
+else
+{
+sum += atoi(argv[i]);
+}
+}
+}
+}
 printf("%d\n", sum);
 return (0);
 }
