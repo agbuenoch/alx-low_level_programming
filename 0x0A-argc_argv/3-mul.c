b36d2fc1760b47bd11f3f3f0c8236efde7e63df9
agbuenoch <agbuenoch38@gmail.com>
2022-10-01 19:06:39 +0000

    argc and argv commits

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,15 @@ int main(int argc, char **argv)
 {
 int i;
 int mul = 1;
-if (argc == 1)
-  printf("Error\n");
-if(argc > 1)
+ if(argc > 1)
   {
     for (i = 1; i < argc; i++)
       mul *= atoi(argv[i]);
     printf("%d\n", mul);
   }
+ else
+   {
+     printf("Error\n");
+   }
 return (0);
 }
