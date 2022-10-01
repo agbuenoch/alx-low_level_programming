28f6d6152409ba9b1768fb8d7ba70e03e3835dad
agbuenoch <agbuenoch38@gmail.com>
2022-10-01 16:22:08 +0000

    argc and argv commits

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print number of arguement passed to main function
+ *
+ * @argc: integer parameter
+ * @argv: pointer-to-pointer-to-character parameter
+ *
+ * Return: zero
+*/
+int main (int argc, char *argv[])
+{
+  printf("%d\n", argc);
+return (0);
+}
