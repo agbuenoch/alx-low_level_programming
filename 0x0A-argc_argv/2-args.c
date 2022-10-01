136d8147208fee6752c62e1939ed61149316f2a6
agbuenoch <agbuenoch38@gmail.com>
2022-10-01 17:33:45 +0000

    argc and argv commits

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
+ * main - print all arguement passed to main function
  *
+ * @argc: integer parameter
+ * @argv: pointer-to-pointer array parameter
  *
- *
+ * Return: zero
 */
 int main(int argc, char **argv)
 {
-  while (argc--)
-    {
-      printf("%s\n", *argv++);
-    }
-  return (0);
+while (argc--)
+{
+printf("%s\n", *argv++);
+}
+return (0);
 }
