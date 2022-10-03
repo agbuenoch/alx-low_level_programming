4272b0666c520256c6397715f5ad4ca78836057b
agbuenoch <agbuenoch38@gmail.com>
2022-10-03 19:52:55 +0000

    malloc and free commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * _strdup - function that returns a pointer to a newly allocated
  * space in memory, which contains a copy of the string given as a parameter
@@ -10,6 +11,28 @@
 */
 char *_strdup(char *str)
 {
-
-
+  char *arr;
+  int i;  
+ 
+  int j;
+  for (j = 0; str[j] != '\0'; j++)
+    {
+      ;
+    }
+  if (str == NULL)
+    {
+      return (NULL);
+    }
+  
+  arr = malloc(j * sizeof(*arr));
+  if (arr == NULL)
+    {
+      return (NULL);
+    }
+  
+  for (i = 0; str[i] != '\0'; i++)
+    {
+      arr[i] = str[i];
+    }
+  return (arr);
 }
