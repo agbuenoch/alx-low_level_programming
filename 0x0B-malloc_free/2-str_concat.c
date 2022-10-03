e8195454081f564bb87720b22b18283e5c47b1cf
agbuenoch <agbuenoch38@gmail.com>
2022-10-03 21:20:52 +0000

    malloc and free commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,46 +11,41 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-  char *arr;
-  int i;
-  int j;
-
-  int a;
-  int index;
-  index = 0;
-
-  if (s1 == NULL)
-    {
-      s1 = "";
-    }
-  if (s2 == NULL)
-    {
-      s2 = "";
-    }
-  
-  for (i = 0; s1[i] != '\0'; i++)
-    {
-      ;
-    }
-  for (j = 0; s2[j] != '\0'; j++)
-    {
-      ;
-    }
-  
-  arr = malloc(((i + j) + 1) * sizeof(*arr));
-  if (arr == NULL)
-    {
-      return (NULL);
-    }
-  for (a = 0; s1[a] != '\0'; a++, index++)
-    {
-      arr[index] = s1[a];
-    }
-
-  for (a = 0; s2[a] != '\0'; a++, index++)
-    {
-      arr[index] = s2[a];
-    }
-  arr[index] = '\0';
-  return (arr);
+char *arr;
+int i;
+int j;
+int a;
+int index;
+index = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+;
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+;
+}
+arr = malloc(((i + j) + 1) * sizeof(*arr));
+if (arr == NULL)
+{
+return (NULL);
+}
+for (a = 0; s1[a] != '\0'; a++, index++)
+{
+arr[index] = s1[a];
+}
+for (a = 0; s2[a] != '\0'; a++, index++)
+{
+arr[index] = s2[a];
+}
+arr[index] = '\0';
+return (arr);
 }
