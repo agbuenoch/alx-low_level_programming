269aca8dac1b546dc3f0978924b905a648631c9f
agbuenoch <agbuenoch38@gmail.com>
2022-10-09 19:26:40 +0000

    More malloc and free projects commits

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * _calloc - function that allocates memory for an array, using malloc
  *
@@ -10,6 +11,28 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+  int *arr;
+  
 
-
+  arr = malloc(nmemb * size);
+  if (arr == NULL)
+    {
+      return (NULL);
+    }
+  else
+    {
+      while (*arr != '\0')
+	{
+	  *arr++ = 0;
+	}
+    }
+  if (nmemb == 0 || size == 0)
+    {
+      return (NULL);
+    }
+  while (nmemb--)
+    {
+      *arr++ = nmemb;
+    }
+  return (arr);
 }
