af967b228dcaa8332545163e057372ac76c64642
agbuenoch <agbuenoch38@gmail.com>
2022-10-10 22:59:13 +0000

    Structures and Typedef commits

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * init_dog - function that initialize a variable of type struct dog
  *
@@ -12,6 +13,11 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+  d = malloc(sizeof(d));
+  if (d == NULL)
+    {
+      ;
+    }
 d->name = name;
 d->age = age;
 d->owner = owner;
