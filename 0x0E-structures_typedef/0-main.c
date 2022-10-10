8a5250e63a763ad3d10c510ae1d03447419acc03
agbuenoch <agbuenoch38@gmail.com>
2022-10-10 22:22:05 +0000

    Structures and Typedef commits

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 struct dog my_dog;
 my_dog.name = "Poppy";
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - attributes of a dog
+ *
+ * @name: first pointer-to-character parameter
+ * @age: second parameter
+ * @owner: second pointer-to-character parameter
+ *
+ * DESCRIPTION: Dogs have four legs and it is a
+ * friendly domestic animal.
+*/
 struct dog
 {
 char *name;
