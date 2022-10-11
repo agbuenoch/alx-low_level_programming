86d655648ddac1a78f9a56143172289bc54e0924
agbuenoch <agbuenoch38@gmail.com>
2022-10-11 10:31:21 +0000

    Structures and Typedef commits

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
   /*d = malloc(sizeof(struct dog));*/
-  if ((d == NULL))
-printf("Ok\n");
-d->name = name;
-d->age = age;
-d->owner = owner;
+  if (d->name == NULL && &d->age == NULL && d->owner == NULL)
+    printf("Ok\n");
+
+  d->name = name;
+  d->age = age;
+  d->owner = owner;
 }
