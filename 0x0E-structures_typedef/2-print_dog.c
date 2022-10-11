1c56ff115be552c4b8a1b0e5e7a5961699c8d80b
agbuenoch <agbuenoch38@gmail.com>
2022-10-11 20:32:14 +0000

    Structures and Typedef commits

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,39 +4,36 @@
 /**
  * print_dog - function that prints a struct dog
  *
- * @d: pointer-to-struct dog 
+ * @d: pointer-to-struct dog
 */
 void print_dog(struct dog *d)
 {
-  if (d == NULL)
-    {
-      ;
-    }
-
-  if (d->name == NULL)
-    {
-      printf("(nil)\n");
-    }
-  else
-    {
-      printf("Name: %s\n", d->name);
-    }
-
-  if (&d->age == NULL)
-    {
-      printf("(nil)\n");
-    }
-  else
-    {
-      printf("Age: %.6f\n", d->age);
-    }
-
-  if (d->owner == NULL)
-    {
-      printf("(nil)\n");
-    }
-  else
-    {
-      printf("Owner: %s\n", d->owner);
-    }
+if (d == NULL)
+{
+;
+}
+if (d->name == NULL)
+{
+printf("(nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+if (&d->age == NULL)
+{
+printf("(nil)\n");
+}
+else
+{
+printf("Age: %.6f\n", d->age);
+}
+if (d->owner == NULL)
+{
+printf("(nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
 }
