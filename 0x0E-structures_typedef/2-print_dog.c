991407d8085066c652db49136b179310e1f3e26e
agbuenoch <agbuenoch38@gmail.com>
2022-10-11 20:23:37 +0000

    Structures and Typedef commits

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,9 +8,35 @@
 */
 void print_dog(struct dog *d)
 {
-  /* d = malloc(sizeof(struct dog));*/
-printf("Name: %s\n", d->name);
- printf("Age: %.6f\n", d->age);
- printf("Owner: %s\n", d->owner);
-    
+  if (d == NULL)
+    {
+      ;
+    }
+
+  if (d->name == NULL)
+    {
+      printf("(nil)\n");
+    }
+  else
+    {
+      printf("Name: %s\n", d->name);
+    }
+
+  if (&d->age == NULL)
+    {
+      printf("(nil)\n");
+    }
+  else
+    {
+      printf("Age: %.6f\n", d->age);
+    }
+
+  if (d->owner == NULL)
+    {
+      printf("(nil)\n");
+    }
+  else
+    {
+      printf("Owner: %s\n", d->owner);
+    }
 }
