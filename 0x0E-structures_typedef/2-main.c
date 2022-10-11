fd93a93f84a1b70f72957227bf443bdc5899587d
agbuenoch <agbuenoch38@gmail.com>
2022-10-11 21:07:09 +0000

    Structures and Typedef commits

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 struct dog my_dog;
-my_dog.name = NULL;
+my_dog.name = "Poppy";
 my_dog.age = 4.750000;
-my_dog.owner = NULL;
+my_dog.owner = "Bob";
 print_dog(&my_dog);
 return (0);
 }
