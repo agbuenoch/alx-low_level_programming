11c6d1a1fc0aa5639673c6fb1675b045fc8e060e
agbuenoch <agbuenoch38@gmail.com>
2022-10-12 11:34:37 +0000

    Function pointers commits

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * print_name - prints a name
  *
+ * @name: pointer-to-character parameter
+ * @f: pointer-to-function parameter
+ *
+ * Return: nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
