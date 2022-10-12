7b42b0a6e352139513842b7de79f978a5785d6cb
agbuenoch <agbuenoch38@gmail.com>
2022-10-12 12:28:18 +0000

    Function pointers commits

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,13 +15,13 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 size_t i;
 if (array == NULL)
 {
-;
+exit(EXIT_SUCCESS);
 }
 if (size == 0)
 {
-;
+exit(EXIT_SUCCESS);
 }
- for (i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 action(array[i]);
 }
