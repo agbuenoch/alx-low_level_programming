86f9465445ea7fa947125fc214efe70e59718095
agbuenoch <agbuenoch38@gmail.com>
2022-10-12 16:12:37 +0000

    Structures and Typedef commits

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
  * new_dog - function that creates a new dog.
- * 
+ *
  * @name: pointer-to-character
  * @age: float parameter
  * @owner: pointer-to-character
@@ -14,12 +14,12 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *ptr;
-  ptr = malloc(sizeof(dog_t));
-  if (ptr == NULL)
-      return (NULL);
-  ptr->name = name;
-  ptr->age = age;
-  ptr->owner = owner;
-  return (ptr);
+dog_t *ptr;
+ptr = malloc(sizeof(dog_t));
+if (ptr == NULL)
+return (NULL);
+ptr->name = name;
+ptr->age = age;
+ptr->owner = owner;
+return (ptr);
 }
