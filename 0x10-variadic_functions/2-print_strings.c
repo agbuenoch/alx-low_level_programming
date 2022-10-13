aff77e89f456c872a5142b82b77ac47700b02e54
agbuenoch <agbuenoch38@gmail.com>
2022-10-13 13:27:35 +0000

    Variadic functions commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,12 +21,16 @@ exit(EXIT_SUCCESS);
 
  for (i = 0; i < n; i++)
    {
-      printf("%s", va_arg(ap, char*));
+     if (va_arg(ap, char*) == NULL)
+       printf("(nil)");
+     else
+       printf("%s", va_arg(ap, char*));
+       
       if (i < n - 1)
 	{
 	  printf("%s", separator);
 	}
    }
- va_end(ap);
+va_end(ap);
 putchar('\n');
 }
