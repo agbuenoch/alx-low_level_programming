ee1cc2ec6015e2ec9a9be4e1967a341ec1b86c66
agbuenoch <agbuenoch38@gmail.com>
2022-10-24 09:54:21 +0000

    Linked list commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * first - prints a string
+ *
+ * Return: always zero 
+*/
+int first()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+return (0)
+}
