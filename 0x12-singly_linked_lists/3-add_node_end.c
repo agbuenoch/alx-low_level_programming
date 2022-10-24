3b9126b7a17e7cf6937227fbf3542f9362317b58
agbuenoch <agbuenoch38@gmail.com>
2022-10-24 09:05:30 +0000

    Linked list commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,45 +12,42 @@
 */
 int str_len(const char *str)
 {
-  int len;
-  len = 0;
-  while (*str)
-    {
-      len++;
-      str++;
-    }
-  return (len);
+int len;
+len = 0;
+while (*str)
+{
+len++;
+str++;
+}
+return (len);
 }
 list_t *add_node_end(list_t **head, const char *str)
 {
-  int len;
-  list_t *new_node;
-  list_t *last_node;
-
-  len = str_len(str);
-  new_node = malloc(sizeof(list_t) * (len + 1));
-  if (new_node == NULL)
-    {
-      return (NULL);
-    }
-  new_node->str = strdup(str);
-  if (str == NULL)
-    {
-      free(new_node);
-      return (NULL);
-    }
-
-  new_node->len = len;
-  new_node->next = NULL;
-  
-  if (*head == NULL)
-      *head = new_node;
-  else
-    {
-      last_node = *head;
-      while (last_node->next != NULL)
-	  last_node = last_node->next;
-      last_node->next = new_node;
-    }
-  return (*head);
+int len;
+list_t *new_node;
+list_t *last_node;
+len = str_len(str);
+new_node = malloc(sizeof(list_t) * (len + 1));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->str = strdup(str);
+if (str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->len = len;
+new_node->next = NULL;
+if (*head == NULL)
+*head = new_node;
+else
+{
+last_node = *head;
+while (last_node->next != NULL)
+last_node = last_node->next;
+last_node->next = new_node;
+}
+return (*head);
 }
