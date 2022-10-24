bfc95161c3222e8d4920c8a49c9ea5ed399e7317
agbuenoch <agbuenoch38@gmail.com>
2022-10-24 20:15:44 +0000

    More linked list commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * listint_len - function that returns the number of elements
+ * in a linked listint_t list.
+ *
+ * @h: pointer-to-listint_t
+ *
+ * Return: size_t
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t no_elements;
+no_elements = 0;
+while (h)
+{
+h = h->next;
+no_elements++;
+}
+return (no_elements);
+}
