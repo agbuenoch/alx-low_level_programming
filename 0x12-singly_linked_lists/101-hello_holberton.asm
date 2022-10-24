769e8c546644d0f5468c82d9889c0f988fde20ed
agbuenoch <agbuenoch38@gmail.com>
2022-10-24 16:07:11 +0000

    Linked list commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,19 @@
-global main
-	section .text:
+
+extend printf
+
+section .text:
+	global main
+
 main:
-	mov eax, 0x4
-	mov ebx, 1
-	mov ecx, message
-	mov edx, message_length
-	int 0x80
-				; now gracefully exit
-	mov eax, 0x1
-	mov ebx, 0
-	int 0x80
-	section .data:
-	message: db "Hello, Holberton", 0xA
-	message_length equ $-message
+	push rbp
+	mov rdi,format
+	mov rsi,message
+	mov rax,0
+	call printf
+	pop rbp
+	mov rax,0
+	ret
+
+section .data
+	message: db "Hello, Holberton", 0
+	format:	 db "%s", 10, 0
