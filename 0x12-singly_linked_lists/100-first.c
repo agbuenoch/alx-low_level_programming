142f3c4c03e944ba9a78989931b0df1164dfea34
agbuenoch <agbuenoch38@gmail.com>
2022-10-24 10:07:04 +0000

    Linked list commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,10 @@ void __attribute__((constructor)) first(void);
 /**
  * first - prints a string
  *
- * Return: always zero 
+ * Return: always zero
 */
 void first()
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore my \
+house upon my back!\n");
 }
