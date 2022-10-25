a53c05a56ee37fd8b05f5badf97a58d3fedf8763
agbuenoch <agbuenoch38@gmail.com>
2022-10-25 11:10:11 +0000

    More linked list commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint_end - function that adds a new node
  * at the end of a listint_t list.
- * 
+ *
  * @head: pointer-to-pointer-to-listint_t
  * @n: constant integer parameter
  *
@@ -11,29 +11,25 @@
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-  listint_t *last_node;
-  listint_t *new_node;
-  
-  new_node = malloc(sizeof(listint_t));
-  if (new_node == NULL)
-    {
-      return (NULL);
-    }
-  
-  last_node = *head;
-  new_node->n = n;
-  new_node->next = NULL;
-  
-  if (*head == NULL)
-    {
-      *head = new_node;
-    }
-  
-  while (last_node->next != NULL)
-    {
-      last_node = last_node->next;
-    }
-  last_node->next = new_node;
-  return (*head);
-
+listint_t *last_node;
+listint_t *new_node;
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+last_node = *head;
+new_node->n = n;
+new_node->next = NULL;
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
+while (last_node->next != NULL)
+{
+last_node = last_node->next;
+}
+last_node->next = new_node;
+return (new_node);
 }
