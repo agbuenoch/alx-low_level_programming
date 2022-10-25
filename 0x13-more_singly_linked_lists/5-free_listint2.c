2cbe4b8bf92d50889626b217fd56f397f08e41b5
agbuenoch <agbuenoch38@gmail.com>
2022-10-25 14:46:47 +0000

    More linked list commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
 void free_listint2(listint_t **head)
 {
 listint_t *temp = *head;
-if(*head == NULL)
+if (*head == NULL)
 {
 free(*head);
 }
@@ -18,4 +18,5 @@ temp = temp->next;
 free(*head);
 *head = temp;
 }
+*head = NULL;
 }
