43df985f5916cc57b11bd7904b79398089926db5
agbuenoch <agbuenoch38@gmail.com>
2022-10-26 12:06:14 +0000

    More linked list commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
   unsigned int count;
   listint_t *new_node;
-  /*listint_t *temp_2;*/
+  listint_t *head_copy;
+
+  head_copy = *head;
 
   new_node = malloc(sizeof(listint_t));
   if (new_node == NULL)
@@ -21,26 +23,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
       return (NULL);
     }
   new_node->n = n;    
-  for (count = 0; count < idx; count++)
+  for (count = 0; count < (idx - 1); count++)
     {
-      if (*head == NULL)
+      if (head_copy == NULL)
 	{
 	  return (NULL);
 	}
-      if (!(idx < count))
-	{
-	  return (NULL);
-	}
-      else
-	{
-      *head = (*head)->next;
-	}
-   
-      new_node = *head;
+     
+      head_copy = head_copy->next;
     }
-      /*
-      *head = new_node;
-       temp_2 = temp;
-      temp = temp->next;*/
+  new_node->next = head_copy->next;
+  head_copy->next = new_node;
+
   return (new_node);
 }
