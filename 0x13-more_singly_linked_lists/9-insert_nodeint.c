53fdcc8cb94cf9c6d30ecb031d938ee66d1a090e
agbuenoch <agbuenoch38@gmail.com>
2022-10-26 10:53:02 +0000

    More linked list commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
       temp = (*head)->next;
     }
-  temp = temp->next;
+  /* temp = temp->next;*/
   return (temp);
 }
