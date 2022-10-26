ae2de73e9742c7cfda84baa34c320dd44a49600c
agbuenoch <agbuenoch38@gmail.com>
2022-10-26 12:43:29 +0000

    More linked list commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position
+ * insert_nodeint_at_index - function that inserts a new node
+ * at a given position
  *
  * @head: pointer-to-pointer-to-listint_t
  * @idx: integer parameter
@@ -11,38 +12,31 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-  unsigned int node;
-  listint_t *new_node;
-  listint_t *head_copy;
-
-  head_copy = *head;
-
-  new_node = malloc(sizeof(listint_t));
-  if (new_node == NULL)
-    {
-      return (NULL);
-    }
-  new_node->n = n;    
-
-  if (idx == 0)
-    {
-      new_node->next = head_copy;
-      *head = new_node;
-      return (new_node);
-    }
-  for (node = 0; node < (idx - 1); node++)
-    {
-      if (head_copy == NULL || head_copy->next == NULL)
-	{
-	  return (NULL);
-	}
-     
-      head_copy = head_copy->next;
-    }
-  new_node->next = head_copy->next;
-  head_copy->next = new_node;
-  
-  /*  head_copy->next = new_node;
-      new_node = new_node->next;*/
-  return (new_node);
+unsigned int node;
+listint_t *new_node;
+listint_t *head_copy;
+head_copy = *head;
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+if (idx == 0)
+{
+new_node->next = head_copy;
+*head = new_node;
+return (new_node);
+}
+for (node = 0; node < (idx - 1); node++)
+{
+if (head_copy == NULL || head_copy->next == NULL)
+{
+return (NULL);
+}
+head_copy = head_copy->next;
+}
+new_node->next = head_copy->next;
+head_copy->next = new_node;
+return (new_node);
 }
