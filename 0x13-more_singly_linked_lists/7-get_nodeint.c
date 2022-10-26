ee7422b744ae6942cc8a934a5b3eca2525ce84d7
agbuenoch <agbuenoch38@gmail.com>
2022-10-26 08:59:49 +0000

    More linked list commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,14 +11,14 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-  unsigned int count;
-  for (count = 0; count < index; count++)
-    {
-      if (head == NULL)
-	{
-	  return (NULL);
-	}
-      head = head->next;
-    }
-  return (head);
+unsigned int count;
+for (count = 0; count < index; count++)
+{
+if (head == NULL)
+{
+return (NULL);
+}
+head = head->next;
+}
+return (head);
 }
