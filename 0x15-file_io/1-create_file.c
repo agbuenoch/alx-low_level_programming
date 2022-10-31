9dac6398a7742c3393a5ef1226c19b3d4f50a92a
agbuenoch <agbuenoch38@gmail.com>
2022-10-31 20:00:25 +0000

    File I/O Commits

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,14 +26,24 @@ int create_file(const char *filename, char *text_content)
       return (-1);
     }
 
-  fd = open(filename, O_CREAT | O_WRONLY, 0600);
-      
-  if (fd == -1)
-    {
-      return (-1);
-    }
 
-  write(fd, text_content, length + 1);
-  
+      if (text_content == NULL)
+	{
+	  fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	    if (fd == -1)
+	      {
+	       return (-1);
+	      }
+	    }
+      else
+	{
+	  fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	  if (fd == -1)
+	    {
+	      return (-1);
+	    }
+	  write(fd, text_content, length + 1);
+	}
+      
   return (1);
 }
