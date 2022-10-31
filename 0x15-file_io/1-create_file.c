f3ed7c61d8bcf5f836a3cf37e78653977414c773
agbuenoch <agbuenoch38@gmail.com>
2022-10-31 17:32:36 +0000

    File I/O Commits

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ *
+ * @filename: constant character-pointer
+ * @text_context: character-pointer
+ *
+ * Return: integer
+*/
+int create_file(const char *filename, char *text_content)
+{
+  int fd;
+  int length;
+  char *err = "fails";
+
+  length = 0;
+  
+  if (filename == NULL)
+    {
+      return (-1);
+    }
+
+  if (filename)
+    {
+      fd = open(filename, O_RDWR);
+    }
+  else
+    {
+      fd = open(filename, O_RDWR, 0600);
+    }
+  if (fd == -1)
+    {
+      while (*err)
+	{
+	  _putchar(*err);
+	  err++;
+	}
+	  return (-1);
+    }
+
+  while (*text_content)
+    {
+      length++;
+      text_content++;
+    }
+  write(fd, text_content, length);
+  
+  return (1);
+}
