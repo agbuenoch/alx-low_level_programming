f76c35235cb78d73e0a574fb0c078526bd167132
agbuenoch <agbuenoch38@gmail.com>
2022-10-31 13:09:20 +0000

    File I/O Commits

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,32 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * 
+ * read_textfile - function that reads a text file
+ * and prints it to the POSIX standard output
+ *
+ * @filename: constant character-pointer
+ * @letters: size_t parameter
+ *
+ * Return: ssize_t
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+  int fd;
+  char *read_into;
 
+  read_into = malloc(sizeof(char) * letters);
+  
+  fd = open(filename, O_RDONLY);
+  if (fd == -1)
+    {
+      return (0);
+    }
 
+  read(fd, read_into, letters);
+  read_into = '\0';
+  close(fd);
+  
+  return (store);
 }
