95622138bfd92bc07dd0df8701d0e98a0655ba41
agbuenoch <agbuenoch38@gmail.com>
2022-10-31 23:12:55 +0000

    File I/O Commits

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,31 +11,29 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  char *buffer;
-  ssize_t open_file;
-  ssize_t read_file;
-  ssize_t write_file;
-
-  buffer = malloc(sizeof(char) * letters);
-  if (buffer == NULL)
-    {
-      return (0);
-    }
-  if (filename == NULL)
-    {
-      return (0);
-    }
-  open_file = open(filename, O_RDONLY);
-  read_file = read(open_file, buffer, letters);
-  write_file = write(STDOUT_FILENO, buffer, read_file);
-  if (open_file == -1 || read_file == -1 || write_file == -1 || write_file != read_file)
-    {
-      free(buffer);
-      return (0);
-    }
-
-  free(buffer);
-  close(open_file);
-  
-  return (write_file);
+char *buffer;
+ssize_t open_file;
+ssize_t read_file;
+ssize_t write_file;
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+return (0);
+}
+if (filename == NULL)
+{
+return (0);
+}
+open_file = open(filename, O_RDONLY);
+read_file = read(open_file, buffer, letters);
+write_file = write(STDOUT_FILENO, buffer, read_file);
+if (open_file == -1 || read_file == -1 || write_file == -1 \
+    || write_file != read_file)
+{
+free(buffer);
+return (0);
+}
+free(buffer);
+close(open_file);
+return (write_file);
 }
