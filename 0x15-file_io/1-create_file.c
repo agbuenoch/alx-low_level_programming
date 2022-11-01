d32e290543ccdbbec7498251f6004765299a92ca
agbuenoch <agbuenoch38@gmail.com>
2022-11-01 08:15:35 +0000

    File I/O Commits

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,10 @@ int create_file(const char *filename, char *text_content)
 
   length = 0;
   
-  while (*text_content)
+  if (text_content == NULL)
     {
-      length++;
-      text_content++;
+      for (length = 0; text_content[length];)
+	length++;
     }
 
   if (filename == NULL)
