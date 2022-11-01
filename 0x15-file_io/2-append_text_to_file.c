30423efd4d293d4afa6dba7bd7a19afaf4534db8
agbuenoch <agbuenoch38@gmail.com>
2022-11-01 10:05:28 +0000

    File I/O Commits

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,37 +9,25 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-
-  int open_file;
-  int write_file;
-  int length;
-
-  length = 0;
-
-  if (text_content != NULL)
-    {
-      for (;text_content[length];)
-	length++;
-    }
-
-  if (filename == NULL)
-    {
-      return (-1);
-    }
-  /*
-  if ((text_content == NULL) | (access(filename, F_OK) != 0))
-    {
-      return (-1);
-    }*/
-
-  open_file = open(filename, O_WRONLY | O_APPEND);
-  write_file = write(open_file, text_content, length);
-
-  if (open_file == -1 || write_file == -1)
-    {
-      return (-1);
-    }
-  
-  close (open_file);
-  return (1);
+int open_file;
+int write_file;
+int length;
+length = 0;
+if (text_content != NULL)
+{
+for (;text_content[length];)
+length++;
+}
+if (filename == NULL)
+{
+return (-1);
+}
+open_file = open(filename, O_WRONLY | O_APPEND);
+write_file = write(open_file, text_content, length);
+if (open_file == -1 || write_file == -1)
+{
+return (-1);
+}
+close (open_file);
+return (1);
 }
