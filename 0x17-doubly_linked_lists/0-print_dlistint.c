4b99e27c814f55e7eeb2b026f266800d841bb197
agbuenoch <agbuenoch38@gmail.com>
2022-12-30 21:52:01 +0000

    Hello world commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,7 @@
  *   * @h: The head of the dlistint_t list.
  *    *
  *     * Return: The number of nodes in the list.
- *      */
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
